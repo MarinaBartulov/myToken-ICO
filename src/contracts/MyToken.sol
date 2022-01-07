8d81914098b3ff195a709f352fae88664cd4fb1d
MarinaBartulov <bartulovmarina1@gmail.com>
2022-01-07 23:36:57 +0100

    delegated transfer function implemented

--- src/contracts/MyToken.sol
+++ src/contracts/MyToken.sol
@@ -8,13 +8,18 @@ contract MyToken {
 	string public standard = "MyToken v1.0"; //not part of ERC20 standard
 
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
+	//First address: Account A
+	//Second address: Account B, Account C, Account D,...
+	//I (Account A) approve Account B to spend uint256 tokens on my behalf
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
 	constructor(uint256 _initialSupply) public {
+		// allocate initial supply to admin
 		balanceOf[msg.sender] = _initialSupply;
-		totalSupply = _initialSupply; 
-		// allocate initial supply to admin 
+		totalSupply = _initialSupply;  
 	}
 
 	function transfer(address _to, uint256 _value) public returns (bool success){
@@ -24,5 +29,28 @@ contract MyToken {
 		emit Transfer(msg.sender, _to, _value);
 		return true;
 	}
+
+	// delegated transfers
+	// 2 step process:
+	// one function will allow our account to approve a transfer
+	// another function will handle the delegated transfer 
+
+	// I want to approve account _spender to spend _value on my behalf
+	function approve(address _spender, uint256 _value) public returns (bool success){
+		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+		require(balanceOf[_from] >= _value, 'Error, there is no enough amount to transfer.');
+		require(allowance[_from][msg.sender] >= _value, 'Error, allowance is not big enough for transfer'); //msg.sender is third-party
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		allowance[_from][msg.sender] -= _value;
+		emit Transfer(_from, _to, _value);
+		return true;
+	}
+
 }
 
--- test/MyToken.js
+++ test/MyToken.js
@@ -51,11 +51,59 @@ contract("MyToken", (accounts) => {
 				expect(success).to.eq(true)
 			})
 
+			it('testing approving tokens for delegated transfer...', async() => {
+				const success = await myToken.approve.call(accounts[1], 100);
+				expect(success).to.eq(true)
+				const receipt = await myToken.approve(accounts[1], 100, { from: accounts[0]});
+				expect(receipt.logs.length).to.eq(1)
+				expect(receipt.logs[0].event).to.be.eq('Approval')
+				expect(receipt.logs[0].args._owner).to.be.eq(accounts[0])
+				expect(receipt.logs[0].args._spender).to.be.eq(accounts[1])
+				expect(Number(receipt.logs[0].args._value)).to.eq(100)
+				expect(Number(await myToken.allowance(accounts[0], accounts[1]))).to.eq(100)
+			})
+
+			it('testing delegated transfer...', async() => {
+				const fromAccount = accounts[2];
+				const toAccount = accounts[3];
+				const spendingAccount = accounts[4];
+				// I need to transfer some tokens to fromAccount
+				await myToken.transfer(fromAccount, 100, { from: accounts[0]});
+				// Approve spendingAccount to spend 10 tokens from fromAccount
+				await myToken.approve(spendingAccount, 10, { from: fromAccount});
+				//this doesn't change the state of blockchain
+				const success = await myToken.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount});
+				expect(success).to.eq(true)
+				//this changes the state of blockchain
+				const receipt = await myToken.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
+				expect(receipt.logs.length).to.eq(1)
+				expect(receipt.logs[0].event).to.be.eq('Transfer')
+				expect(receipt.logs[0].args._from).to.be.eq(accounts[2])
+				expect(receipt.logs[0].args._to).to.be.eq(accounts[3])
+				expect(Number(receipt.logs[0].args._value)).to.eq(10)
+				expect(Number(await myToken.balanceOf(fromAccount))).to.eq(90)
+				expect(Number(await myToken.balanceOf(toAccount))).to.eq(10)
+				expect(Number(await myToken.allowance(fromAccount, spendingAccount))).to.eq(0)
+
+			})
+
 		})
 
 		describe('failure', () => {
 			it('transfer function should be rejected...', async() => {
 				await myToken.transfer.call(accounts[1], 99999999999).should.be.rejectedWith(EVM_REVERT)
+			})
+
+			it('delegated transfer should be rejected...', async() => {
+				const fromAccount = accounts[2];
+				const toAccount = accounts[3];
+				const spendingAccount = accounts[4];
+				// I need to transfer some tokens to fromAccount
+				await myToken.transfer(fromAccount, 100, { from: accounts[0]});
+				// Approve spendingAccount to spend 10 tokens from fromAccount
+				await myToken.approve(spendingAccount, 10, { from: fromAccount});
+				await myToken.transferFrom(fromAccount, toAccount, 9999, {from: spendingAccount}).should.be.rejectedWith(EVM_REVERT);
+				await myToken.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount}).should.be.rejectedWith(EVM_REVERT);
 			})
 		})
 	})
