619a5a9e45c3aeb621e42893408f391a97e3bf98
MarinaBartulov <bartulovmarina1@gmail.com>
2022-01-08 16:11:44 +0100

    feature for buying tokens implemented

--- src/contracts/MyTokenSale.sol
+++ src/contracts/MyTokenSale.sol
@@ -7,10 +7,36 @@ contract MyTokenSale {
 	address admin; // don't want to expose the admin address
 	MyToken public tokenContract;
 	uint256 public tokenPrice;
+	uint256 public tokensSold;
+
+	event Sell(address _buyer, uint256 _amount);
 
 	constructor(MyToken _tokenContract, uint256 _tokenPrice) public {
 		admin = msg.sender;
 		tokenContract = _tokenContract;
 		tokenPrice = _tokenPrice;
+	}
+
+	// pure - not doing any transaction, not interacting with blockchain
+	function multiply(uint x, uint y) internal pure returns (uint z){
+		require(y == 0 || (z = x * y) / y == x);
+	}
+
+	//payable - want someone to be able to send ether via transaction with this function
+	function buyTokens(uint256 _numberOfTokens) public payable {
+		// require that value is equal to tokens cost
+		require(msg.value == multiply(_numberOfTokens, tokenPrice), 'Error, not enough ether sent.');
+
+		// require that the contract has enough tokens
+		require(tokenContract.balanceOf(address(this))>=_numberOfTokens, 'Error, not enough tokens available.');
+
+		// require that a transfer is successful - to revert if it doesn't happen
+		require(tokenContract.transfer(msg.sender, _numberOfTokens), 'Error, transaction for buying tokens failed.');
+
+		// keep track of tokensSold
+		tokensSold += _numberOfTokens;
+
+		// trigger Sell Event
+		emit Sell(msg.sender, _numberOfTokens);
 	}
 }
--- test/MyTokenSale.js
+++ test/MyTokenSale.js
@@ -1,13 +1,21 @@
+import { EVM_REVERT } from './helpers'
+
 const MyTokenSale = artifacts.require("MyTokenSale");
 const MyToken = artifacts.require("MyToken");
 
 contract("MyTokenSale", (accounts) => {
 	let myTokenSale, myToken
+	let admin = accounts[0] //contains all tokens at the beginning 
+	let buyer = accounts[1]
 	let tokenPrice = 1000000000000000; // in wei, this is 0.001 ETH, each token will cost 0.001 ETH
+	let tokensAvailabe = 750000;
 
 	beforeEach(async () => {
 		myToken = await MyToken.new(1000000) 
 		myTokenSale = await MyTokenSale.new(myToken.address, tokenPrice)
+		// Provision 75% of all tokens to the token sale
+		await myToken.transfer(myTokenSale.address, tokensAvailabe, { from: admin})
+
 	})
 
 	describe('testing MyTokenSale contract', () => {
@@ -22,6 +30,40 @@ contract("MyTokenSale", (accounts) => {
 				expect(Number(await myTokenSale.tokenPrice())).to.eq(tokenPrice)
 			})
 			
+		})
+	})
+
+	describe('testing tokens buying...', () => {
+		describe('success', () => {
+			it('checking tokens sold number...', async () => {
+				let numberOfTokens = 10;
+				let value = numberOfTokens * tokenPrice;
+				const receipt = await myTokenSale.buyTokens(numberOfTokens, { from: buyer, value: value})
+				expect(Number(await myTokenSale.tokensSold())).to.eq(numberOfTokens);
+				expect(receipt.logs.length).to.eq(1)
+				expect(receipt.logs[0].event).to.be.eq('Sell')
+				expect(receipt.logs[0].args._buyer).to.be.eq(buyer)
+				expect(Number(receipt.logs[0].args._amount)).to.eq(numberOfTokens)	
+				// number of buyer's tokens should increase
+				expect(Number(await myToken.balanceOf(buyer))).to.eq(numberOfTokens)
+				// number of available tokens should decrease
+				expect(Number(await myToken.balanceOf(myTokenSale.address))).to.eq(tokensAvailabe - numberOfTokens)		
+			})
+			
+		})
+
+		describe('failure', () => {
+			let numberOfTokens = 10;
+			it("testing buying tokens different from the ether value...", async () => {
+				let value = numberOfTokens * tokenPrice; // how much wei is sent
+				// try with 1 wei to buy 10 tokens
+				await myTokenSale.buyTokens(numberOfTokens, { from: buyer, value: 1}).should.be.rejectedWith(EVM_REVERT); 
+			})
+
+			it("testing buying tokens more than available...", async () => {
+				let value = numberOfTokens * tokenPrice; 
+				await myTokenSale.buyTokens(800000, { from: buyer, value: value}).should.be.rejectedWith(EVM_REVERT); 
+			})
 		})
 	})
 })
